e36a2679aa3206ee3f3768c1220e0a03be181258
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-14 23:02:51 +0530

    1st done

--- jan23 long/farFromOrigin.cpp
+++ jan23 long/farFromOrigin.cpp
@@ -9,8 +9,14 @@ int main()
     {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        int d1 = sqrt(x1 * x1 + y1 * y1);
-        int d2 = sqrt(x2 * x2 + y2 * y2);
+        int d1 = ((x1 * x1) + (y1 * y1));
+        int d2 = ((x2 * x2) + (y2 * y2));
+        if (d1 > d2)
+            cout << "ALEX" << endl;
+        else if (d1 == d2)
+            cout << "EQUAL" << endl;
+        else
+            cout << "BOB" << endl;
     }
 
     return 0;
