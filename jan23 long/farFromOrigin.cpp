f999f74f33e15fa0db26b6903f7d3a8e4920540c
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-14 22:51:27 +0530

    1st qs init y

--- jan23 long/farFromOrigin.cpp
+++ jan23 long/farFromOrigin.cpp
@@ -9,6 +9,8 @@ int main()
     {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
+        int d1 = sqrt(x1 * x1 + y1 * y1);
+        int d2 = sqrt(x2 * x2 + y2 * y2);
     }
 
     return 0;
